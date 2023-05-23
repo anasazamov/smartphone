07dafc028eb4c55175f77070a6f227954656e4de
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-23 18:35:30 +0500

    register phone model

--- smartphone/admin.py
+++ smartphone/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Phone
 
-# Register your models here.
+model_list = [Phone]
+
+admin.site.register(model_list)
--- smartphone/models.py
+++ smartphone/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Phone(models.Model):
+    name        = models.CharField(max_length=255)
+    description = models.TextField(default='', blank=True)
+    url         = models.URLField(unique=True)
+    color       = models.CharField(max_length=20)
+    ram         = models.IntegerField()
+    memory      = models.IntegerField()
+    brend       = models.CharField(max_length=20)
+    price       = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.name} - {self.brend}'
+    
